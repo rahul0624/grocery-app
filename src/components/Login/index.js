f154c6238c7dc09f0e82b9a8ff96c6af78b55ccd
Rahul <rahul.suddala@gmail.com>
2022-03-25 15:59:09 +0530

    auth functionality added

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,10 +1,20 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./styles/index.css";
 
 const Login = () => {
   const [user, setUser] = useState({
     email: "",
     password: "",
   });
+
+  const [loginStatus, setLoginStatus] = useState(false);
+
+  const [loginFail, setLoginFail] = useState(false);
+
+  useEffect(() => {
+    console.log();
+  }, [loginStatus, loginFail]);
 
   const onChangeHandler = (event) => {
     setUser({
@@ -13,33 +23,62 @@ const Login = () => {
     });
   };
 
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    axios
+      .post("https://apolis-grocery.herokuapp.com/api/auth/login", user)
+      .then((response) => {
+        console.log(response.data);
+        if (response.data.token) {
+          setLoginStatus(true);
+          setLoginFail(false);
+          sessionStorage.setItem("token", response.data.token);
+        }
+      })
+      .catch((err) => {
+        setLoginStatus(false);
+        setLoginFail(true);
+      });
+  };
+
   return (
-    <div className="flex justify-content-center">
+    <div className="flex justify-content-center" id="login-form-wrap">
       <h1 className="text-center">Login</h1>
-      <form className="flex justify-content-center col-6">
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
+      {loginStatus && (
+        <div className="alert alert-primary" role="alert">
+          Logged In Successfully!
+        </div>
+      )}
+      {loginFail && (
+        <div className="alert alert-danger" role="alert">
+          Incorrect Credentials!
+        </div>
+      )}
+      <form
+        id="login-form"
+        className="flex justify-content-center"
+        onSubmit={onSubmitHandler}
+      >
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email</label>
           <input
             type="email"
             name="email"
-            class="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
+            className="form-control"
             onChange={onChangeHandler}
           />
         </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
           <input
             type="password"
-            class="form-control"
+            className="form-control"
             name="password"
-            id="exampleInputPassword1"
             onChange={onChangeHandler}
           />
         </div>
 
-        <button type="submit" class="btn btn-primary">
+        <button id="login" type="submit" className="btn btn-primary">
           Login
         </button>
       </form>
--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,36 +1,63 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
 
 const Register = () => {
+  const [user, setUser] = useState({
+    firstName: "",
+    email: "",
+    mobile: "",
+    password: "",
+  });
 
-    const [user, setUser] = useState({
-        firstName: '',
-        email: '',
-        mobile: '',
-        password: ''
-    })
+  const [registerStatus, setRegisterStatus] = useState(false);
 
-    const onChangeHandler = (event) => {
-        setUser({
-            ...user,
-            [event.target.name] : event.target.value
-        })
-    }
+  const [registerFail, setRegisterFail ] = useState(false);
 
-    const onSubmitHandler = (event) => {
-      event.preventDefault();
-      axios.post('https://apolis-grocery.herokuapp.com/api/auth/register',user)
-      .then(response => {
+  const onChangeHandler = (event) => {
+    setUser({
+      ...user,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  useEffect(()=>{
+    console.log('status Changed');
+  },[registerStatus,registerFail]);
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    axios
+      .post("https://apolis-grocery.herokuapp.com/api/auth/register", user)
+      .then((response) => {
         console.log(response);
+        if (!response.error) {
+          setRegisterStatus(true);
+          setRegisterFail(false);
+        }
       })
-      .catch(error => console.log(error));
-    }
+      .catch((error) => {
+        setRegisterFail(true);
+        setRegisterStatus(false);
+        console.log(error);
+      });
+  };
 
   return (
-    <div className="flex justify-content-center">
-        <h1 className="text-center">Register</h1>
-      <form className="col-6" onSubmit={onSubmitHandler}>
-      <div className="form-group">
+    <div id="reg-form-wrap">
+      <h1 className="text-center">Register</h1>
+      {registerStatus && (
+        <div className="alert alert-primary" role="alert">
+          Registered Successfully Please Login!
+        </div>
+      )}
+      {registerFail && (
+        <div className="alert alert-danger" role="alert">
+          Registraion Failed! User already Exists.
+        </div>
+      )}
+      <form onSubmit={onSubmitHandler} id="login-form">
+        <div className="form-group">
           <label>Name</label>
           <input
             type="text"
@@ -53,7 +80,7 @@ const Register = () => {
           />
         </div>
         <div className="form-group">
-          <label >Mobile</label>
+          <label>Mobile</label>
           <input
             type="text"
             className="form-control"
@@ -64,7 +91,7 @@ const Register = () => {
           />
         </div>
         <div className="form-group">
-          <label >Password</label>
+          <label>Password</label>
           <input
             type="password"
             name="password"
